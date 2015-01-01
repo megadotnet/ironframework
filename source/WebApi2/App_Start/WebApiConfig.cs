32cfcfd8cda86e2833a74efe99d4e75e182a305a
Peter Liu <wintersunair@gmail.com>
2015-01-01 21:08:28 +0800

    Updated entlib config files

--- source/WebApi2/App_Start/WebApiConfig.cs
+++ source/WebApi2/App_Start/WebApiConfig.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace WebApi2
@@ -19,6 +20,8 @@ namespace WebApi2
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
