f1bf5627bd25cdaa5a029edb68b75538269f0613
Peter Liu <wintersunair@gmail.com>
2015-02-01 11:49:54 +0800

    Added AutoFixture supported

---
+++ source/WebApi2.Tests/Controllers/EmployeeControllerTests.cs
@@ -0,0 +1,30 @@
+﻿using BusinessEntiies;
+using Ploeh.AutoFixture.Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi2.Controllers;
+using Xunit;
+using Xunit.Extensions;
+
+namespace WebApi2.Tests.Controllers
+{
+    public class EmployeeControllerTests
+    {
+        /// <summary>
+        /// AutoFixture test
+        /// </summary>
+        /// <seealso cref="https://github.com/AutoFixture/AutoFixture"/>
+        /// <param name="employeeDto"></param>
+        //[Theory, AutoData]
+        public void TestMethod(EmployeeDto employeeDto)
+        {
+            var controller = new EmployeeController();
+            //var result=controller.Get(employeeDto);
+
+            //Assert.NotNull(result);
+        }
+    }
+}
