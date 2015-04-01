77639ef63ea1969a4a835adf686e304752b70907
Megadotnet <wintersunair@gmail.com>
2015-04-01 21:21:16 +0800

    Added async unit tests

---
+++ source/MvcApp.Tests/Controllers/AsyncContronlerTests.cs
@@ -0,0 +1,31 @@
+﻿using MVC5Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MvcApp.Tests.Controllers
+{
+    public class AsyncContronlerTests
+    {
+        /// <summary>
+        /// TestAsync
+        /// </summary>
+        /// <returns></returns>
+        /// <see cref="http://bradwilson.typepad.com/blog/2012/01/xunit19.html"/>
+        [Fact]
+        public async Task TestAsync()
+        {
+            //
+            var controller = new EmployeeController();
+            var result=await controller.Get(1);
+
+            Assert.NotNull(result.Data);
+       
+        }
+
+    }
+}
