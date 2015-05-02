b5650db84309a1d511a8e7869a093714a1b096bc
Megadotnet <wintersunair@gmail.com>
2015-05-02 11:38:54 +0800

    Added Interceptor For Unity

---
+++ source/Utility/EntLib/Interceptor/LoggingInterceptor.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronFramework.Utility.PolicyInjection.Interceptor
+{
+    /// <summary>
+    /// LoggingInterceptor
+    /// </summary>
+    public class LoggingInterceptor : IInterceptionBehavior
+    {
+
+        /// <summary>
+        /// The log
+        /// </summary>
+        private ILogger log = new Logger("LoggingInterceptor");
+
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            /* Call the method that was intercepted */
+            string className = input.MethodBase.DeclaringType.Name;
+            string methodName = input.MethodBase.Name;
+            string generic = input.MethodBase.DeclaringType.IsGenericType ? string.Format("<{0}>", input.MethodBase.DeclaringType.GetGenericArguments().ToStringList()) : string.Empty;
+            string arguments = input.Arguments.ToStringList();
+
+            string preMethodMessage = string.Format("{0}{1}.{2}({3})", className, generic, methodName, arguments);
+            log.Debug("PreMethodCalling: " + preMethodMessage);
+            //Logging
+            log.Debug(preMethodMessage);
+            //Invoke method
+            IMethodReturn msg = null;
+        
+            msg = getNext()(input, getNext);
+            //loggin exception
+            if (msg.Exception != null)
+            {
+                log.Error(msg.Exception);
+            }
+
+            //Post method calling
+            string postMethodMessage = string.Format("{0}{1}.{2}() -> {3}", className, generic, methodName, msg.ReturnValue);
+            log.Debug("PostMethodCalling: " + postMethodMessage);
+            //Logging
+            log.Debug(postMethodMessage);
+            return msg;
+        }
+
+        public bool WillExecute
+        {
+            get { return true; }
+        }
+    }
+
+    public static class EnumerableExtensions
+    {
+        public static string ToStringList(this IEnumerable list)
+        {
+            var sb = new StringBuilder();
+
+            foreach (var item in list)
+            {
+                sb.AppendFormat("{0}, ", item);
+            }
+            if (sb.Length > 0)
+                sb.Remove(sb.Length - 2, 2);
+
+            return sb.ToString();
+        }
+    }
+
+}
---
+++ source/Utility/EntLib/Interceptor/PerformanceInterceptor.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronFramework.Utility.PolicyInjection.Interceptor
+{
+
+    /// <summary>
+    /// PerformanceInterceptor
+    /// </summary>
+    public class PerformanceInterceptor : IInterceptionBehavior
+    {
+        /// <summary>
+        /// The log
+        /// </summary>
+        private static readonly ILogger log = new Logger("PerformanceInterceptor");
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PerformanceInterceptor"/> class.
+        /// </summary>
+        public PerformanceInterceptor()
+        {
+        }
+
+        /// <summary>
+        /// Returns the interfaces required by the behavior for the objects it intercepts.
+        /// </summary>
+        /// <returns>
+        /// The required interfaces.
+        /// </returns>
+        public IEnumerable<Type> GetRequiredInterfaces()
+        {
+            return Type.EmptyTypes;
+        }
+
+        /// <summary>
+        /// Implement this method to execute your behavior processing.
+        /// </summary>
+        /// <param name="input">Inputs to the current call to the target.</param>
+        /// <param name="getNext">Delegate to execute to get the next delegate in the behavior chain.</param>
+        /// <returns>
+        /// Return value from the target.
+        /// </returns>
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextInterceptionBehaviorDelegate getNext)
+        {
+            string className = input.MethodBase.DeclaringType.Name;
+            string methodName = input.MethodBase.Name;
+
+            IMethodReturn msg = null;
+
+            //Pre method calling
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+           
+            // Method is invoked
+            msg = getNext()(input, getNext);
+
+            //Post method calling
+            stopwatch.Stop();
+            log.DebugFormat("{2}－{1}Time elapsed: {0} TotalSeconds", stopwatch.Elapsed.TotalSeconds, methodName, className);
+            return msg;
+        }
+
+        /// <summary>
+        /// Returns a flag indicating if this behavior will actually do anything when invoked.
+        /// </summary>
+        /// <remarks>
+        /// This is used to optimize interception. If the behaviors won't actually
+        /// do anything (for example, PIAB where no policies match) then the interception
+        /// mechanism can be skipped completely.
+        /// </remarks>
+        public bool WillExecute
+        {
+            get
+            {
+                return true;
+            }
+
+        }
+    }
+}
