1018e6c22bfd087327b3d6ed7bf19c39296a29d7
Megadotnet <wintersunair@gmail.com>
2015-05-17 21:03:01 +0800

    Fixed Bugs in UI and connected UI and WebAPI

--- source/MVC5Web/Controllers/ControllerBase.cs
+++ source/MVC5Web/Controllers/ControllerBase.cs
@@ -11,55 +11,301 @@
 
 namespace MVC5Web.Controllers
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using System.Web.Mvc;
-    using BusinessObject;
+    using DataServiceClient;
+using IronFramework.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
     /// <summary>
     /// a interface of data acccess repository.
     /// </summary>
     public class BaseWebController : Controller
     {
-        #region Public Methods
+       #region Public WebApi client Methods
 
+        /// <summary>
+        /// Gets or sets the webapi URI.
+        /// </summary>
+        /// <value>
+        /// The webapi URI.
+        /// </value>
+        public string webapiURI
+        { 
+            get; 
+            set; 
+        }
+
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseWebController"/> class.
+        /// </summary>
+        public BaseWebController()
+        {
+            this.webapiURI=ServiceConfig.URI;
+        }
+
+        #region Wrap REST CRUD method API
+        /// <summary>
+        /// Clients the invoke HTTP post.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model">The model.</param>
+        /// <returns></returns>
         public async Task<bool> ClientInvokeHttpPOST<T>(T model) where T : new()
         {
-            return await RESTAPIClinet.Instance().ClientInvokeHttpPOST<T>(model);
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHttpPOST<T>(model);
         }
 
-        public async Task<T> ClientHTTPGet<T>(int id) where T:new()
+        /// <summary>
+        /// Clients the invoke HTTP post.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="query">The query.</param>
+        /// <param name="customURL">The custom URL.</param>
+        /// <returns></returns>
+        public async Task<bool> ClientInvokeHttpPOST<T, Query>(Query query, string customURL) where T : new()
         {
-           return await RESTAPIClinet.Instance().ClientHTTPGet<T>(id);
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHttpPOST<T, Query>(query, customURL);
         }
 
-        public async Task<T> ClientHTTPGetList<T, Query>(Query query) where T : new()
+        /// <summary>
+        /// Clients the invoke HTTP post.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="query">The query.</param>
+        /// <param name="customURL">The custom URL.</param>
+        /// <param name="isawait">if set to <c>true</c> [isawait].</param>
+        /// <returns></returns>
+        public async Task<bool> ClientInvokeHttpPOST<T, Query>(Query query, string customURL, bool isawait) where T : new()
         {
-            return await RESTAPIClinet.Instance().ClientHTTPGetList<T, Query>(query);
-        }
-		        
-	    public async Task<T> ClientHTTPGetList<T, Query>(string querystring) where T : new()
-        {
-            return await RESTAPIClinet.Instance().ClientHTTPGetList<T, Query>(querystring);
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHttpPOST<T, Query>(query, customURL, isawait);
         }
 
+        /// <summary>
+        /// Clients the invoke HTTP post.
+        /// </summary>
+        /// <typeparam name="ReturnObject">The type of the eturn object.</typeparam>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="query">The query.</param>
+        /// <param name="customURL">The custom URL.</param>
+        /// <returns></returns>
+        public async Task<ReturnObject> ClientInvokeHttpPOST<ReturnObject, T, Query>(Query query, string customURL)
+            where T : new()
+            where ReturnObject : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHttpPOST<ReturnObject, T, Query>(query, customURL);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGet<TResult>(int id) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGet<TResult>(id);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="query">The query.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGetList<TResult, Query>(Query query) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGetList<TResult, Query>(query);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="query">The query.</param>
+        /// <param name="isAwait">if set to <c>true</c> [is await].</param>
+        /// <returns></returns>
+        public async Task<T> ClientHTTPGetList<T, Query>(Query query, bool isAwait) where T : new()
+        {
+            return await RESTAPIWrapperClinet.Instance().ClientHTTPGetList<T, Query>(query, isAwait);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="partialURI">The partial URI.</param>
+        /// <param name="query">The query.</param>
+        /// <param name="isAwait">if set to <c>true</c> [is await].</param>
+        /// <returns></returns>
+        public async Task<T> ClientHTTPGetList<T, Query>(string partialURI, Query query, bool isAwait) where T : new()
+        {
+            return await RESTAPIWrapperClinet.Instance().ClientHTTPGetList<T, Query>(partialURI, query, isAwait);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TQueryDto">The type of the query dto.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="partialURI">The partial URI.</param>
+        /// <param name="query">The query.</param>
+        /// <param name="isAwait">if set to <c>true</c> [is await].</param>
+        /// <returns></returns>
+        public async Task<T> ClientHTTPGetList<T, TQueryDto, Query>(string partialURI, Query query, bool isAwait) where T : new()
+        {
+            return await RESTAPIWrapperClinet.Instance().ClientHTTPGetList<T, TQueryDto, Query>(partialURI, query, isAwait);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="partialURI">The partial URI.</param>
+        /// <param name="query">The query.</param>
+        /// <returns></returns>
+        public async Task<T> ClientHTTPGetList<T, Query>(string partialURI, Query query) where T : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGetList<T, Query>(query, partialURI);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="queryString">The query string.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGetList<TResult, Query>(string queryString) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGetList<TResult, Query>(queryString);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="partialURI">The partial URI.</param>
+        /// <param name="isawait">if set to <c>true</c> [isawait].</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGetList<TResult, Query>(string partialURI, bool isawait) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGetList<TResult, Query>(partialURI, isawait);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="partialURI">The partial URI.</param>
+        /// <param name="queryString">The query string.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGetList<TResult, Query>(string partialURI, string queryString) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGetList<TResult, Query>(partialURI, queryString);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get list.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="partialURI">The partial URI.</param>
+        /// <param name="queryString">The query string.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGetList<TResult, Query>(string partialURI, string queryString, bool isawait) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGetList<TResult, Query>(partialURI, queryString, isawait);
+        }
+
+        /// <summary>
+        /// Clients the HTTP get.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <param name="id">The identifier.</param>
+        /// <param name="customURL">The custom URL.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPGet<TResult>(int id, string customURL) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPGet<TResult>(id, customURL);
+        }
+
+        /// <summary>
+        /// Clients the HTTP put.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="model">The model.</param>
+        /// <returns></returns>
         public async Task<bool> ClientHTTPPut<T>(T model) where T : new()
         {
-            return await RESTAPIClinet.Instance().ClientHTTPPut<T>(model);
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPPut<T>(model);
         }
 
-        public async Task<bool> ClientHTTPDelete<T>(int id)
-        {
-            return await RESTAPIClinet.Instance().ClientHTTPDelete<T>(id);
-        }
 
+        /// <summary>
+        /// Clients the HTTP put.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="model">The model.</param>
+        /// <param name="customPartialUri">The custom partial URI.</param>
+        /// <returns></returns>
         public async Task<bool> ClientHTTPPut<TResult, Query>(Query model, string customPartialUri)
         {
-            return await RESTAPIClinet.Instance().ClientHTTPPut<TResult, Query>(model, customPartialUri);
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPPut<TResult, Query>(model, customPartialUri);
         }
-       
+
+        /// <summary>
+        /// Clients the HTTP put.
+        /// </summary>
+        /// <typeparam name="TResult">The type of the result.</typeparam>
+        /// <typeparam name="TQueryDto">The type of the query dto.</typeparam>
+        /// <typeparam name="Query">The type of the uery.</typeparam>
+        /// <param name="model">The model.</param>
+        /// <param name="customPartialUri">The custom partial URI.</param>
+        /// <returns></returns>
+        public async Task<TResult> ClientHTTPPut<TResult, TQueryDto, Query>(Query model, string customPartialUri) where TResult : new()
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPPut<TResult, TQueryDto, Query>(model, customPartialUri);
+        }
+
+        /// <summary>
+        /// Clients the HTTP delete.
+        /// </summary>
+        /// <typeparam name="T">T name should include DTOs string</typeparam>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        public async Task<bool> ClientHTTPDelete<T>(int id)
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPDelete<T>(id);
+        }
+
+        /// <summary>
+        /// Clients the HTTP delete.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="id">The identifier.</param>
+        /// <param name="customPartialUri">The custom partial URI.</param>
+        /// <returns></returns>
+        public async Task<bool> ClientHTTPDelete<T>(int id, string customPartialUri)
+        {
+            return await RESTAPIWrapperClinet.Instance(this.webapiURI).ClientHTTPDelete<T>(id, customPartialUri);
+        } 
+        #endregion
+
         #endregion
     }
 
