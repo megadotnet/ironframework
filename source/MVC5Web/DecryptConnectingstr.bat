d4c100d8e722dfe54395e62f7c4c9f83825ebc2e
Megadotnet <wintersunair@gmail.com>
2015-09-04 11:24:48 +0800

    Encrypt connection string

---
+++ source/MVC5Web/DecryptConnectingstr.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+ASPNET_REGIIS -pdf "remoteDataAPI" "J:\MyProject\GitHub\ironframework\source\MVC5Web"
+ASPNET_REGIIS -pdf "connectionStrings" "J:\MyProject\GitHub\ironframework\source\MVC5Web"
