33ec969f10965a36cae09967c3e0ed830e0ae7e8
Megadotnet <wintersunair@gmail.com>
2015-09-07 20:39:28 +0800

    Removed encrypt connection string

--- source/MVC5Web/DecryptConnectingstr.bat
+++
@@ -1,4 +0,0 @@
-C:
-cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
-ASPNET_REGIIS -pdf "remoteDataAPI" "J:\MyProject\GitHub\ironframework\source\MVC5Web"
-ASPNET_REGIIS -pdf "connectionStrings" "J:\MyProject\GitHub\ironframework\source\MVC5Web"
---
+++ source/MVC5Web/EncryptConnectingstr.bat
@@ -0,0 +1,4 @@
+C:
+cd C:\Windows\Microsoft.NET\Framework\v4.0.30319
+ASPNET_REGIIS -pef "remoteDataAPI" "J:\MyProject\GitHub\ironframework\source\MVC5Web"
+ASPNET_REGIIS -pef "connectionStrings" "J:\MyProject\GitHub\ironframework\source\MVC5Web"
