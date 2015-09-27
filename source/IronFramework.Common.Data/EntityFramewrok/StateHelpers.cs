3e627b00b0030248f2a9f97ce93e9dbc048ce214
Megadotnet <wintersunair@gmail.com>
2015-09-27 14:08:55 +0800

    Refactoring

---
+++ source/IronFramework.Common.Data/EntityFramewrok/StateHelpers.cs
@@ -0,0 +1,78 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="StateHelpers.cs" company="Megadotnet">
+//   Copyright (c) 2010-2011 Petter Liu.  All rights reserved. 
+// </copyright>
+// <summary>
+//   The state for entity
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IronFramework.Common.Data.EntityFramewrok
+{
+    using System.Data.Entity;
+
+    /// <summary>
+    /// The state.
+    /// </summary>
+    public enum State
+    {
+        /// <summary>
+        /// The unchanged.
+        /// </summary>
+        Unchanged, 
+
+        /// <summary>
+        /// The added.
+        /// </summary>
+        Added, 
+
+        /// <summary>
+        /// The modified.
+        /// </summary>
+        Modified, 
+
+        /// <summary>
+        /// The deleted.
+        /// </summary>
+        Deleted,
+
+        Detached
+    }
+
+    /// <summary>
+    /// The state helpers.
+    /// </summary>
+    public static class StateHelpers
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// The get equivalent entity state.
+        /// </summary>
+        /// <param name="state">
+        /// The state.
+        /// </param>
+        /// <returns>
+        /// Entity State
+        /// </returns>
+        /// <remarks>
+        /// this handy method comes from Rowan Miller on the EF team
+        /// </remarks>
+        public static EntityState GetEquivalentEntityState(State state)
+        {
+            switch (state)
+            {
+                case State.Added:
+                    return EntityState.Added;
+                case State.Modified:
+                    return EntityState.Modified;
+                case State.Deleted:
+                    return EntityState.Deleted;
+                default:
+                    return EntityState.Unchanged;
+            }
+        }
+
+        #endregion
+    }
+}
