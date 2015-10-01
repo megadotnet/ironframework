2c7172c573a739db749d9beb9ca8e074117a5be5
Megadotnet <wintersunair@gmail.com>
2015-10-01 10:24:47 +0800

    Added IronFramework.Common.Config project

---
+++ source/IronFramework.Common.Config/SectionHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace IronFramework.Common.Config
+{
+    /// <summary>
+    /// SectionHandler
+    /// </summary>
+    public class SectionHandler<T> : System.Configuration.IConfigurationSectionHandler
+        where T : BaseConfig
+    {
+        /// <summary>
+        /// Creates a configuration section handler.
+        /// </summary>
+        /// <param name="parent">Parent object.</param>
+        /// <param name="configContext">Configuration context object.</param>
+        /// <param name="section">Section XML node.</param>
+        /// <returns>
+        /// The created section handler object.
+        /// </returns>
+        public object Create(object parent, object configContext, System.Xml.XmlNode section)
+        {
+           var t=(T)Activator.CreateInstance(typeof(T), section);
+           
+            return t;
+        }
+    }
+}
---
+++ source/IronFramework.Common.Config/ServiceConfig.cs
@@ -0,0 +1,67 @@
+﻿namespace IronFramework.Common.Config
+{
+    using System;
+
+
+ 
+    public class ServiceConfig : BaseConfig
+    {
+
+        public ServiceConfig(System.Xml.XmlNode node)
+            : base(node)
+        {
+
+        }
+
+
+        static ServiceConfig()
+        {
+            ConfigSectionName = typeof(ServiceConfig).Name;
+        }
+
+        /// <summary>
+        /// Gets the mq ip address.
+        /// </summary>
+        /// <value>
+        /// The mq ip address.
+        /// </value>
+        public static string SectionName
+        {
+            get
+            {
+                return configSection["SectionName"];
+            }
+        }
+
+        /// <summary>
+        /// Gets the URI.
+        /// </summary>
+        /// <value>
+        /// The URI.
+        /// </value>
+        public static string URI
+        {
+            get
+            {
+                return configSection["URI"];
+            }
+        }
+
+
+        /// <summary>
+        /// Gets the access control URI.
+        /// </summary>
+        /// <value>
+        /// The access control URI.
+        /// </value>
+        public static string AccessControlURI
+        {
+            get
+            {
+                return configSection["AccessControlURI"];
+            }
+        }
+
+
+    }
+}
