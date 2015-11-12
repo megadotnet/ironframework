a6cce13b1af92665d5e9a8a48a0b9336585ed6ac
Megadotnet <wintersunair@gmail.com>
2015-11-12 21:33:05 +0800

    Updated unity to version 4.1

--- source/MVC5Web/App_Start/UnityMvcActivator.cs
+++ source/MVC5Web/App_Start/UnityMvcActivator.cs
@@ -3,6 +3,7 @@ using System.Web.Mvc;
 using Microsoft.Practices.Unity.Mvc;
 
 [assembly: WebActivatorEx.PreApplicationStartMethod(typeof(MVC5Web.App_Start.UnityWebActivator), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethod(typeof(MVC5Web.App_Start.UnityWebActivator), "Shutdown")]
 
 namespace MVC5Web.App_Start
 {
@@ -21,6 +22,13 @@ namespace MVC5Web.App_Start
 
             // TODO: Uncomment if you want to use PerRequestLifetimeManager
             // Microsoft.Web.Infrastructure.DynamicModuleHelper.DynamicModuleUtility.RegisterModule(typeof(UnityPerRequestHttpModule));
+        }
+
+        /// <summary>Disposes the Unity container when the application is shut down.</summary>
+        public static void Shutdown()
+        {
+            var container = UnityConfig.GetConfiguredContainer();
+            container.Dispose();
         }
     }
 }
