3be2da25dabaecbbf979e2c031f5cdce65eda2d2
Megadotnet <wintersunair@gmail.com>
2017-05-26 22:09:51 +0800

    Integrated Swagger with dotnet core webapi

--- source/DotCoreWebAPI/Startup.cs
+++ source/DotCoreWebAPI/Startup.cs
@@ -12,6 +12,7 @@
     using static DotCoreWebAPI.Program;
     using Microsoft.EntityFrameworkCore;
     using DotCoreWebAPI.Models2;
+    using Swashbuckle.AspNetCore.Swagger;
 
     public class Startup
     {
@@ -36,6 +37,26 @@
 
             // Add framework services.
             services.AddMvc();
+
+            // Register the Swagger generator, defining one or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc(
+                    "v1", 
+                    new Info
+                    {
+                        Title = "IronFramework dotnet core http API"
+                    ,
+                        Version = "v1"
+                    ,
+                        Contact = new Swashbuckle.AspNetCore.Swagger.Contact
+                        {
+                            Name = "Megadotnet", Email = "kmdxdk1@hotmail.com", Url = "https://github.com/megadotnet/ironframework"
+                        }
+                        ,
+                        License = new Swashbuckle.AspNetCore.Swagger.License { Name = "MIT", Url = "https://opensource.org/licenses/MIT" }
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +66,15 @@
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "IronFramework API V1");
+            });
         }
     }
 }
