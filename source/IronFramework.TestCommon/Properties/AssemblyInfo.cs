5785a121d595a96fdeffae1b1a41f3a59e38be85
megadotnet <wintersunair@gmail.com>
2017-06-09 09:27:10 +0800

    Rename TestCommon projects

--- source/IronFramework.TestCommon/Properties/AssemblyInfo.cs
+++ source/IronFramework.TestCommon/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("IronFramework.TestCommon")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Unit testing common Utils")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Megadonet")]
 [assembly: AssemblyProduct("IronFramework.TestCommon")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2015")]
+[assembly: AssemblyCopyright("Copyright © Megadonet 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
