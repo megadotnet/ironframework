63a3714c136aa6b97356e3a01e63b53cece0780d
megadotnet <wintersunair@gmail.com>
2017-07-03 22:21:28 +0800

    Added EmployeeControllerTests

---
+++ source/UnitTest/WebAPI/EmployeeControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using BusinessObject;
+using BusinessObject.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi2.Controllers;
+using Xunit;
+
+namespace UnitTest.WebAPI
+{
+    /// <summary>
+    /// EmployeeControllerTests
+    /// </summary>
+    public class EmployeeControllerTests
+    {
+        [Fact]
+        public void TestGet()
+        {
+            var employeeController = new EmployeeController(new EmployeeBO(new EmployeeConverter()));
+            Assert.NotNull(employeeController);
+
+        }
+    }
+}
