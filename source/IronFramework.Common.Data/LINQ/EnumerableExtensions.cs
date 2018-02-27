ec982241171c0f0bd635160be555aeee61347a90
megadotnet <wintersunair@gmail.com>
2018-02-27 14:30:57 +0800

    Added LinqJoinObject

---
+++ source/IronFramework.Common.Data/LINQ/EnumerableExtensions.cs
@@ -0,0 +1,68 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EnumerableExtensions.cs" company="Megadotnet">
+//  Copyright (c) 2010-2018 Petter Liu.  All rights reserved. 
+// </copyright>
+// <summary>
+//  EnumerableExtensions.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace IronFramework.Common.Data.LINQ
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+
+    /// <summary>
+    /// The enumerable extensions.
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// The full outer join.
+        /// </summary>
+        /// <param name="leftItems">
+        /// The left items.
+        /// </param>
+        /// <param name="leftIdSelector">
+        /// The left id selector.
+        /// </param>
+        /// <param name="rightItems">
+        /// The right items.
+        /// </param>
+        /// <param name="rightIdSelector">
+        /// The right id selector.
+        /// </param>
+        /// <param name="projection">
+        /// The projection.
+        /// </param>
+        /// <typeparam name="TLeft">the TLeft</typeparam>
+        /// <typeparam name="TRight">the TRight</typeparam>
+        /// <typeparam name="TKey">the TKey</typeparam>
+        /// <typeparam name="TResult">the TResult</typeparam>
+        /// <returns>
+        /// The <see cref="IQueryable"/>.
+        /// </returns>
+        public static IQueryable<TResult> FullOuterJoin<TLeft, TRight, TKey, TResult>(
+            this IQueryable<TLeft> leftItems,
+            Expression<Func<TLeft, TKey>> leftIdSelector,
+            IQueryable<TRight> rightItems,
+            Expression<Func<TRight, TKey>> rightIdSelector,
+            Expression<Func<LinqJoinObject<TLeft, TRight>, TResult>> projection)
+        {
+            var leftOuterJoin =
+                leftItems.GroupJoin(rightItems, leftIdSelector, rightIdSelector, (left, temp) => new { left, temp })
+                    .SelectMany(
+                        @t => @t.temp.DefaultIfEmpty(),
+                        (@t, right) => new LinqJoinObject<TLeft, TRight> { Left = @t.left, Right = right });
+
+            var rightOuterJoin =
+                rightItems.GroupJoin(leftItems, rightIdSelector, leftIdSelector, (right, temp) => new { right, temp })
+                    .SelectMany(
+                        @t => @t.temp.DefaultIfEmpty(),
+                        (@t, left) => new LinqJoinObject<TLeft, TRight> { Left = left, Right = @t.right });
+
+            return leftOuterJoin.Union(rightOuterJoin).Select(projection);
+        }
+    }
+}
---
+++ source/IronFramework.Common.Data/LINQ/LinqJoinObject.cs
@@ -0,0 +1,35 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LinqJoinObject.cs" company="Megadotnet Co.,LTD">
+//   Copyright (c) 2010-2018 Petter Liu.  All rights reserved. 
+// </copyright>
+// <summary>
+//   The linq join object.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronFramework.Common.Data.LINQ
+{
+    /// <summary>
+    /// the LinqJoinObject
+    /// </summary>
+    /// <typeparam name="TLeft">the TLeft</typeparam>
+    /// <typeparam name="TRight">the TRight</typeparam>
+    public class LinqJoinObject<TLeft, TRight>
+    {
+        /// <summary>
+        /// Gets or sets the left.
+        /// </summary>
+        public TLeft Left { get; set; }
+
+        /// <summary>
+        /// Gets or sets the right.
+        /// </summary>
+        public TRight Right { get; set; }
+    }
+}
