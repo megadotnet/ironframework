81fcfdfa21e39986077aa548ce1de96b3b3821d3
megadotnet <wintersunair@gmail.com>
2018-03-20 15:50:07 +0800

    Added ExceptionLoggerAttribute class

---
+++ source/IronFramework.Common.IOC/EntLib/CallHandler/ExceptionLoggerAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronFramework.Common.IOC.EntLib.EntLib.CallHandler
+{
+    /// <summary>
+    /// The exception logger attribute.
+    /// </summary>
+    internal class ExceptionLoggerAttribute : HandlerAttribute
+    {
+        /// <summary>
+        /// The create handler.
+        /// </summary>
+        /// <param name="container">
+        /// The container.
+        /// </param>
+        /// <returns>
+        /// The <see cref="ICallHandler"/>.
+        /// </returns>
+
+        public override ICallHandler CreateHandler(IUnityContainer container)
+        {
+            return new ExceptionLoggerCallHandler();
+        }
+    }
+}
---
+++ source/IronFramework.Common.IOC/EntLib/CallHandler/ExceptionLoggerCallHandler.cs
@@ -0,0 +1,58 @@
+﻿using IronFramework.Common.Logging.Logger;
+using Microsoft.Practices.Unity.InterceptionExtension;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronFramework.Common.IOC.EntLib.EntLib.CallHandler
+{
+    /// <summary>
+    /// The exception logger call handler.
+    /// </summary>
+    internal class ExceptionLoggerCallHandler : ICallHandler
+    {
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        private readonly ILogger logger = new Logger(typeof(ExceptionLoggerCallHandler));
+
+        /// <summary>
+        /// Gets or sets the order.
+        /// </summary>
+        public int Order { get; set; }
+
+        /// <summary>
+        /// The invoke.
+        /// </summary>
+        /// <param name="input">
+        /// The input.
+        /// </param>
+        /// <param name="getNext">
+        /// The get next.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IMethodReturn"/>.
+        /// </returns>
+        public IMethodReturn Invoke(IMethodInvocation input, GetNextHandlerDelegate getNext)
+        {
+            IMethodReturn result = getNext()(input, getNext);
+            if (result.Exception != null)
+            {
+                this.logger.Debug("Exception occured: " + result.Exception.Message);
+
+                this.logger.Debug("Parameters:");
+                foreach (var parameter in input.Arguments)
+                {
+                    Console.WriteLine(parameter.ToString());
+                }
+
+                this.logger.Debug("StackTrace:");
+                this.logger.Debug(Environment.StackTrace);
+            }
+
+            return result;
+        }
+    }
+}
