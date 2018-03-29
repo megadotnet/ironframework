34a0bd8b5fb5b5a264eae10ff67e2541395984e8
megadotnet <wintersunair@gmail.com>
2018-03-29 14:08:39 +0800

    Added comments for new IRepostiory interface

--- source/IronFramework.Common.Data.Standard/Abstractions/IQuery.cs
+++ source/IronFramework.Common.Data.Standard/Abstractions/IQuery.cs
@@ -18,19 +18,108 @@ namespace IronFramework.Common.Data.Abstractions
         /// <param name="predicate">The predicate.</param>
         /// <returns></returns>
         IQuery<TEntity> Where(Expression<Func<TEntity, bool>> predicate);
+
+        /// <summary>
+        /// Includes the specified navigation property.
+        /// </summary>
+        /// <param name="navigationProperty">The navigation property.</param>
+        /// <returns></returns>
         IQuery<TEntity> Include(Expression<Func<TEntity, object>> navigationProperty);
+
+        /// <summary>
+        /// Orders the by.
+        /// </summary>
+        /// <param name="keySelector">The key selector.</param>
+        /// <returns></returns>
         IQuery<TEntity> OrderBy(Expression<Func<TEntity, object>> keySelector);
+
+        /// <summary>
+        /// Orders the by descending.
+        /// </summary>
+        /// <param name="keySelector">The key selector.</param>
+        /// <returns></returns>
         IQuery<TEntity> OrderByDescending(Expression<Func<TEntity, object>> keySelector);
+
+        /// <summary>
+        /// Selects the asynchronous.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<IEnumerable<TEntity>> SelectAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Skips the specified skip.
+        /// </summary>
+        /// <param name="skip">The skip.</param>
+        /// <returns></returns>
         IQuery<TEntity> Skip(int skip);
+
+        /// <summary>
+        /// Takes the specified take.
+        /// </summary>
+        /// <param name="take">The take.</param>
+        /// <returns></returns>
         IQuery<TEntity> Take(int take);
+
+        /// <summary>
+        /// Thens the by.
+        /// </summary>
+        /// <param name="thenBy">The then by.</param>
+        /// <returns></returns>
         IQuery<TEntity> ThenBy(Expression<Func<TEntity, object>> thenBy);
+
+        /// <summary>
+        /// Thens the by descending.
+        /// </summary>
+        /// <param name="thenByDescending">The then by descending.</param>
+        /// <returns></returns>
         IQuery<TEntity> ThenByDescending(Expression<Func<TEntity, object>> thenByDescending);
+
+        /// <summary>
+        /// Counts the asynchronous.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<int> CountAsync(CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Firsts the or default asynchronous.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Singles the or default asynchronous.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<TEntity> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Selects the SQL asynchronous.
+        /// </summary>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<IEnumerable<TEntity>> SelectSqlAsync(string sql, object[] parameters, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Anies the asynchronous.
+        /// </summary>
+        /// <param name="predicate">The predicate.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Anies the asynchronous.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<bool> AnyAsync(CancellationToken cancellationToken);
     }
 }
--- source/IronFramework.Common.Data.Standard/Abstractions/IRepository.cs
+++ source/IronFramework.Common.Data.Standard/Abstractions/IRepository.cs
@@ -10,22 +10,119 @@ using System.Threading.Tasks;
 
 namespace IronFramework.Common.Data.Abstractions
 {
+    /// <summary>
+    /// IRepository
+    /// </summary>
+    /// <typeparam name="TEntity">The type of the entity.</typeparam>
     public interface IRepository<TEntity> where TEntity : class
     {
+        /// <summary>
+        /// Finds the asynchronous.
+        /// </summary>
+        /// <param name="keyValues">The key values.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<TEntity> FindAsync(object[] keyValues, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Finds the asynchronous.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="keyValue">The key value.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<TEntity> FindAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Existses the asynchronous.
+        /// </summary>
+        /// <param name="keyValues">The key values.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<bool> ExistsAsync(object[] keyValues, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Existses the asynchronous.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="keyValue">The key value.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<bool> ExistsAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default);
+        /// <summary>
+        /// Loads the property asynchronous.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        /// <param name="property">The property.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task LoadPropertyAsync(TEntity item, Expression<Func<TEntity, object>> property, CancellationToken cancellationToken = default);
+        /// <summary>
+
+        /// Attaches the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
         void Attach(TEntity item);
+
+        /// <summary>
+        /// Detaches the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
         void Detach(TEntity item);
+
+        /// <summary>
+        /// Inserts the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
         void Insert(TEntity item);
+
+        /// <summary>
+        /// Updates the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
         void Update(TEntity item);
+
+        /// <summary>
+        /// Deletes the specified item.
+        /// </summary>
+        /// <param name="item">The item.</param>
         void Delete(TEntity item);
+
+        /// <summary>
+        /// Deletes the asynchronous.
+        /// </summary>
+        /// <param name="keyValues">The key values.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<bool> DeleteAsync(object[] keyValues, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Deletes the asynchronous.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <param name="keyValue">The key value.</param>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns></returns>
         Task<bool> DeleteAsync<TKey>(TKey keyValue, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Queryables this instance.
+        /// </summary>
+        /// <returns></returns>
         IQueryable<TEntity> Queryable();
+
+        /// <summary>
+        /// Queryables the SQL.
+        /// </summary>
+        /// <param name="sql">The SQL.</param>
+        /// <param name="parameters">The parameters.</param>
+        /// <returns></returns>
         IQueryable<TEntity> QueryableSql(string sql, params object[] parameters);
+
+        /// <summary>
+        /// Queries this instance.
+        /// </summary>
+        /// <returns></returns>
         IQuery<TEntity> Query();
     }
 }
