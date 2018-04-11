b3f8088b39570d78f4624d84a3dd501a18c82ffb
megadotnet <wintersunair@gmail.com>
2018-04-11 10:50:37 +0800

    Added Common.Logging.NLog.StandardConsoleTests.csproj

---
+++ source/Common.Logging.NLog.StandardConsoleTests/Program.cs
@@ -0,0 +1,14 @@
+﻿using IronFramework.Common.Logging.Logger;
+using System;
+
+namespace Common.Logging.NLog.StandardConsoleTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger = new Logger("Program");
+            logger.Debug("test with .net core app");
+        }
+    }
+}
